83bcc17ad0828c8c2293e3aa29db7f74c211aefc
mmbukaneshy6 <sheilaegeidza@gmail.com>
2020-04-11 22:25:39 +0300

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { GitComponent } from './git/git.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    GitComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
