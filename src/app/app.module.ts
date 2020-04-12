b1872a80c456277023c3fd480237b8da7468eef3
mmbukaneshy6 <sheilaegeidza@gmail.com>
2020-04-12 21:59:49 +0300

    added card-footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,33 +4,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-// import { GitComponent } from '../app/git/git.component';
-// import { GitService } from '../app/git.service';
-import { FormsModule }from '@angular/forms';
-import {RouterModule } from '@angular/router';
-import { HttpClientModule }from '@angular/common/http';
+
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 import { ProfileComponent } from './profile/profile.component';
-// import { DateCountPipe } from './date-count.pipe';
-// import { HighlightDirective } from './highlight.directive';
-// import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    // GitComponent,
     ProfileComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    // GitService,
     RouterModule,
     FormsModule
-  
-   
+
+
   ],
-  // providers: [GitService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
